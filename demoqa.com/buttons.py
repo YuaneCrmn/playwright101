8d1116e10ea464b8984005820b26f8d0bb9552b5
Yuane Kylle Cremen <cyuanekylle@gmail.com>
2024-01-14 21:45:08 +0800

    started button verification

---
+++ demoqa.com/buttons.py
@@ -0,0 +1,16 @@
+import asyncio
+from playwright.async_api import async_playwright, expect
+
+async def main():
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False)
+        context = await browser.new_context()
+        await context.tracing.start(screenshots=True, snapshots=True, sources=True)
+        page = await context.new_page()
+        await page.goto("https://demoqa.com/buttons")
+        page.pause()
+        await browser.close()
+        
+        
+asyncio.run(main())
+        
