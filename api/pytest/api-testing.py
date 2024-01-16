7103933bc90905455855af69ac0aad448f5bcdde
Yuane Kylle Cremen <cyuanekylle@gmail.com>
2024-01-16 20:31:53 +0800

    get todo api test

--- api/pytest/api-testing.py
+++ api/pytest/api-testing.py
@@ -2,6 +2,11 @@ from typing import Generator
 import pytest
 from playwright.sync_api import Playwright, Page, APIRequestContext, expect
 
+@pytest.fixture(scope="module")
+def myIds():
+    keys = []
+    yield keys
+
 @pytest.fixture(scope="session")
 def api_request_context(
     playwright: Playwright,
@@ -12,11 +17,11 @@ def api_request_context(
     yield request_context
     request_context.dispose()
     
-def test_post_todo(api_request_context: APIRequestContext) -> None:
+def test_post_todo(api_request_context: APIRequestContext, myIds) -> None:
     data = { 
         "completed": False,
-        "title": "test",
-        "id": "500",
+        "title": "test222",
+        "id": "500222",
     }
     new_todo = api_request_context.post(
         "/todos", data=data
@@ -29,4 +34,19 @@ def test_post_todo(api_request_context: APIRequestContext) -> None:
     print("")
     print(f"New Todo request: {new_todo}")
     print(f"todo_response Ver: {todo_response}")
-        
+    
+    myIds.append(todo_response['id'])
+
+def test_get_todo(api_request_context: APIRequestContext, myIds) -> None:
+    get_todo = api_request_context.get(
+        f"/todos/{myIds[0]}"
+    )
+    assert get_todo.ok
+
+    response = get_todo.json()
+
+    assert response['title'] == "test222"
+    assert response['completed'] == False
+
+    print(f"Get todo request: {get_todo}")
+    print(f"Response: {response}")
